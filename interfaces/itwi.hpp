67bcd69c7b745008c4b18b391852c39e99904488
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-09-06 13:37:02 +0200

    New TWI interface, update SPI interface to return status codes

---
+++ interfaces/itwi.hpp
@@ -0,0 +1,16 @@
+#ifndef ITWI_HPP_
+#define ITWI_HPP_
+
+class ITwi {
+   public:
+    virtual ~ITwi() = default;
+
+    virtual uint8_t startTransmission(void) = 0;
+    virtual uint8_t stopTransmission(void) = 0;
+    virtual uint8_t readBytes(uint8_t address, uint8_t bytes[],
+                              size_t numBytes) = 0;
+    virtual uint8_t writeBytes(uint8_t address, uint8_t bytes[],
+                               size_t numBytes) = 0;
+};
+
+#endif /* ITWI_HPP_ */
