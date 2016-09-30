29bc295626a03310d7a2fe5469f8533927875df0
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-09-30 14:33:03 +0200

    Task handle now private

--- os/arduinotask.hpp
+++ os/arduinotask.hpp
@@ -7,15 +7,15 @@
 namespace xXx {
 
 class ArduinoTask {
-   public:
-     virtual ~ArduinoTask();
+  public:
+    virtual ~ArduinoTask();
 
-     virtual void setup() = 0;
-     virtual void loop() = 0;
+    virtual void setup() = 0;
+    virtual void loop() = 0;
 
-     BaseType_t attach(uint16_t stackDepth, UBaseType_t priority);
+    BaseType_t attach(uint16_t stackDepth, UBaseType_t priority);
 
-   protected:
+  private:
     TaskHandle_t _handle = NULL;
 };
 
