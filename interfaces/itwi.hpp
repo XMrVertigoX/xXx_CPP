bb0ad681dcbb68d82663594f5b2eb07ac0af9627
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-11-28 20:55:49 +0100

    Update twi interface

--- interfaces/itwi.hpp
+++ interfaces/itwi.hpp
@@ -5,21 +5,19 @@
 
 namespace xXx {
 
-struct ITwi_Message_t {
-    uint8_t address;
-    uint8_t *txBytes;
-    size_t txNumBytes;
-    uint8_t *rxBytes;
-    size_t rxNumBytes;
-};
-
 enum ITwi_Status_t { ITwi_SUCCESS, ITwi_FAILURE };
+
+typedef void (*ITwi_Callback_t)(ITwi_Status_t status, uint8_t rxBytes[],
+                                size_t rxNumBytes);
 
 class ITwi {
   public:
     virtual ~ITwi() = default;
 
-    virtual ITwi_Status_t transmit_receive(ITwi_Message_t message) = 0;
+    virtual ITwi_Status_t transmit_receive(uint8_t address, uint8_t txBytes[],
+                                           size_t txNumBytes,
+                                           ITwi_Callback_t callback,
+                                           void *user) = 0;
 };
 
 } /* namespace xXx */
