a07cb52a1b4f01bbec6673d0c60e3d9b24dd6081
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-01-05 18:06:11 +0100

    Switch from definitions to enum for memory map

--- components/wireless/RF24/nRF24L01_definitions.h
+++ components/wireless/RF24/nRF24L01_definitions.h
@@ -23,35 +23,64 @@
 #ifndef NRF24L01_DEFINITIONS_H_
 #define NRF24L01_DEFINITIONS_H_
 
+enum RF24_MemoryMap_t {
+    RF24_CONFIG      = 0x00,
+    RF24_EN_AA       = 0x01,
+    RF24_EN_RXADDR   = 0x02,
+    RF24_SETUP_AW    = 0x03,
+    RF24_SETUP_RETR  = 0x04,
+    RF24_RF_CH       = 0x05,
+    RF24_RF_SETUP    = 0x06,
+    RF24_STATUS      = 0x07,
+    RF24_OBSERVE_TX  = 0x08,
+    RF24_CD          = 0x09,
+    RF24_RX_ADDR_P0  = 0x0A,
+    RF24_RX_ADDR_P1  = 0x0B,
+    RF24_RX_ADDR_P2  = 0x0C,
+    RF24_RX_ADDR_P3  = 0x0D,
+    RF24_RX_ADDR_P4  = 0x0E,
+    RF24_RX_ADDR_P5  = 0x0F,
+    RF24_TX_ADDR     = 0x10,
+    RF24_RX_PW_P0    = 0x11,
+    RF24_RX_PW_P1    = 0x12,
+    RF24_RX_PW_P2    = 0x13,
+    RF24_RX_PW_P3    = 0x14,
+    RF24_RX_PW_P4    = 0x15,
+    RF24_RX_PW_P5    = 0x16,
+    RF24_FIFO_STATUS = 0x17,
+    RF24_DYNPD       = 0x1C,
+    RF24_FEATURE     = 0x1D
+};
+
 // clang-format off
 
 /* Memory Map */
-#define CONFIG        (0x00)
-#define EN_AA         (0x01)
-#define EN_RXADDR     (0x02)
-#define SETUP_AW      (0x03)
-#define SETUP_RETR    (0x04)
-#define RF_CH         (0x05)
-#define RF_SETUP      (0x06)
-#define STATUS        (0x07)
-#define OBSERVE_TX    (0x08)
-#define CD            (0x09)
-#define RX_ADDR_P0    (0x0A)
-#define RX_ADDR_P1    (0x0B)
-#define RX_ADDR_P2    (0x0C)
-#define RX_ADDR_P3    (0x0D)
-#define RX_ADDR_P4    (0x0E)
-#define RX_ADDR_P5    (0x0F)
-#define TX_ADDR       (0x10)
-#define RX_PW_P0      (0x11)
-#define RX_PW_P1      (0x12)
-#define RX_PW_P2      (0x13)
-#define RX_PW_P3      (0x14)
-#define RX_PW_P4      (0x15)
-#define RX_PW_P5      (0x16)
-#define FIFO_STATUS   (0x17)
-#define DYNPD         (0x1C)
-#define FEATURE       (0x1D)
+// #define CONFIG        (0x00)
+// #define EN_AA         (0x01)
+// #define EN_RXADDR     (0x02)
+// #define SETUP_AW      (0x03)
+// #define SETUP_RETR    (0x04)
+// #define RF_CH         (0x05)
+// #define RF_SETUP      (0x06)
+// #define STATUS        (0x07)
+// #define OBSERVE_TX    (0x08)
+// #define CD            (0x09)
+// #define RX_ADDR_P0    (0x0A)
+// #define RX_ADDR_P1    (0x0B)
+// #define RX_ADDR_P2    (0x0C)
+// #define RX_ADDR_P3    (0x0D)
+// #define RX_ADDR_P4    (0x0E)
+// #define RX_ADDR_P5    (0x0F)
+// #define TX_ADDR       (0x10)
+// #define RX_PW_P0      (0x11)
+// #define RX_PW_P1      (0x12)
+// #define RX_PW_P2      (0x13)
+// #define RX_PW_P3      (0x14)
+// #define RX_PW_P4      (0x15)
+// #define RX_PW_P5      (0x16)
+// #define FIFO_STATUS   (0x17)
+// #define DYNPD         (0x1C)
+// #define FEATURE       (0x1D)
 
 /* Bit Mnemonics */
 #define MASK_RX_DR    (6)
