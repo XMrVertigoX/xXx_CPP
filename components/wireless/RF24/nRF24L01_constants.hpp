bcc2d92e8f7de6c08ccf5a8992d4a85d018876c1
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-01-06 10:42:18 +0100

    Introduce strongly typed enums

--- components/wireless/RF24/nRF24L01_constants.hpp
+++ components/wireless/RF24/nRF24L01_constants.hpp
@@ -1,5 +1,10 @@
-#ifndef NRF24L01_TYPES_H_
-#define NRF24L01_TYPES_H_
+#ifndef NRF24L01_CONSTANTS_HPP_
+#define NRF24L01_CONSTANTS_HPP_
+
+static const uint8_t max_payload_size   = 32;
+static const uint8_t max_address_length = 5;
+static const uint8_t dummy              = 0xFF;
+static const uint8_t RW_REGISTER_MASK   = 0b00011111;
 
 enum RF24_Command_t {
     RF24_Command_R_REGISTER          = 0x00,
@@ -15,6 +20,22 @@ enum RF24_Command_t {
     RF24_Command_NOP                 = 0xFF,
     RF24_Command_ACTIVATE            = 0x50,
     RF24_Command_REGISTER_MASK       = 0x1F,
+};
+
+// XXX
+enum class RF24_Command : uint8_t {
+    R_REGISTER          = 0x00,
+    W_REGISTER          = 0x20,
+    R_RX_PAYLOAD        = 0x61,
+    W_TX_PAYLOAD        = 0xA0,
+    FLUSH_TX            = 0xE1,
+    FLUSH_RX            = 0xE2,
+    REUSE_TX_PL         = 0xE3,
+    R_RX_PL_WID         = 0x60,
+    W_ACK_PAYLOAD       = 0xA8,
+    W_TX_PAYLOAD_NO_ACK = 0xB0,
+    NOP                 = 0xFF,
+    ACTIVATE            = 0x50 // TODO: Explain from data sheet
 };
 
 enum RF24_MemoryMap_t {
@@ -44,6 +65,36 @@ enum RF24_MemoryMap_t {
     RF24_MM_FIFO_STATUS = 0x17,
     RF24_MM_DYNPD       = 0x1C,
     RF24_MM_FEATURE     = 0x1D
+};
+
+// XXX
+enum class RF24_MemoryMap : uint8_t {
+    CONFIG      = 0x00,
+    EN_AA       = 0x01,
+    EN_RXADDR   = 0x02,
+    SETUP_AW    = 0x03,
+    SETUP_RETR  = 0x04,
+    RF_CH       = 0x05,
+    RF_SETUP    = 0x06,
+    STATUS      = 0x07,
+    OBSERVE_TX  = 0x08,
+    CD          = 0x09,
+    RX_ADDR_P0  = 0x0A,
+    RX_ADDR_P1  = 0x0B,
+    RX_ADDR_P2  = 0x0C,
+    RX_ADDR_P3  = 0x0D,
+    RX_ADDR_P4  = 0x0E,
+    RX_ADDR_P5  = 0x0F,
+    TX_ADDR     = 0x10,
+    RX_PW_P0    = 0x11,
+    RX_PW_P1    = 0x12,
+    RX_PW_P2    = 0x13,
+    RX_PW_P3    = 0x14,
+    RX_PW_P4    = 0x15,
+    RX_PW_P5    = 0x16,
+    FIFO_STATUS = 0x17,
+    DYNPD       = 0x1C,
+    FEATURE     = 0x1D
 };
 
 enum RF24_Config_t {
@@ -125,4 +176,4 @@ enum RF24_SETUP_RETR_t {
 
 // clang-format on
 
-#endif // NRF24L01_TYPES_H_
+#endif // NRF24L01_CONSTANTS_HPP_
