7edbd41f3d0a47baaf5dba23509bb4aa94eb8250
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-02-06 13:55:34 +0100

    Use templates

--- utils/bitoperations.hpp
+++ utils/bitoperations.hpp
@@ -3,64 +3,68 @@
 
 #include <stdint.h>
 
-static inline uint8_t bitwiseAND(uint8_t byte, uint8_t mask) {
-    return (byte & mask);
+// ----- Basic bitwise operations ---------------------------------------------
+
+template <typename TYPE> inline TYPE AND(TYPE byte, TYPE mask) {
+    return (byte bitand mask);
 }
 
-static inline uint64_t bitwiseAND(uint64_t byte, uint64_t mask) {
-    return (byte & mask);
+template <typename TYPE> inline void AND_eq(TYPE byte, TYPE mask) {
+    byte and_eq mask;
 }
 
-static inline void bitwiseAND_r(uint8_t &byte, uint8_t mask) {
-    byte &= mask;
+template <typename TYPE> inline TYPE OR(TYPE byte, TYPE mask) {
+    return (byte bitor mask);
 }
 
-static inline void bitwiseAND_r(uint64_t &byte, uint64_t mask) {
-    byte &= mask;
+template <typename TYPE> inline void OR_eq(TYPE &byte, TYPE mask) {
+    byte or_eq mask;
 }
 
-static inline uint8_t bitwiseOR(uint8_t byte, uint8_t mask) {
-    return (byte | mask);
-}
-
-static inline void bitwiseOR_r(uint8_t &byte, uint8_t mask) {
-    byte |= mask;
-}
-
-static inline uint8_t clearBit(uint8_t &byte, uint8_t bit) {
-    return (byte & ~(1 << bit));
-}
-
-static inline void clearBit_r(uint8_t &byte, uint8_t bit) {
-    byte &= ~(1 << bit);
-}
-
-static inline bool readBit(uint8_t byte, uint8_t bit) {
-    return (byte & (1 << bit));
-}
-
-static inline uint8_t setBit(uint8_t byte, uint8_t bit) {
-    return (byte | (1 << bit));
-}
-
-static inline void setBit_r(uint8_t &byte, uint8_t bit) {
-    byte |= (1 << bit);
-}
-
-static inline uint8_t shiftLeft(uint8_t &byte, uint8_t amount) {
+template <typename TYPE> inline TYPE LEFT(uint8_t byte, uint8_t amount) {
     return (byte << amount);
 }
 
-static inline void shiftLeft_r(uint8_t &byte, uint8_t amount) {
+template <typename TYPE> inline void LEFT_eq(uint8_t &byte, uint8_t amount) {
     byte <<= amount;
 }
 
-static inline uint8_t shiftRight(uint8_t &byte, uint8_t amount) {
+template <typename TYPE> inline TYPE RIGHT(uint8_t byte, uint8_t amount) {
     return (byte >> amount);
 }
 
-static inline void shiftRight_r(uint8_t &byte, uint8_t amount) {
+template <typename TYPE> inline void RIGHT_eq(uint8_t &byte, uint8_t amount) {
     byte >>= amount;
+}
+
+template <typename TYPE> inline TYPE INVERT(TYPE byte) {
+    return (compl(byte));
+}
+
+template <typename TYPE> inline void INVERT_eq(TYPE &byte) {
+    byte = compl(byte);
+}
+
+// ----- Advanced bitwise operations ------------------------------------------
+
+template <typename TYPE> inline TYPE clearBit(TYPE byte, TYPE bit) {
+    return (byte bitand compl(LEFT<TYPE>(1, bit)));
+}
+
+template <typename TYPE> inline void clearBit_eq(TYPE &byte, TYPE bit) {
+    byte bitand compl(LEFT<TYPE>(1, bit));
+}
+
+template <typename TYPE> inline TYPE setBit(TYPE byte, TYPE bit) {
+    return (byte bitor (LEFT<TYPE>(1, bit)));
+}
+
+template <typename TYPE> inline void setBit_eq(TYPE &byte, TYPE bit) {
+    byte or_eq (LEFT<TYPE>(1, bit));
+}
+
+template <typename TYPE> inline bool readBit(TYPE byte, TYPE bit) {
+    return (byte bitand (LEFT<TYPE>(1, bit)));
 }
 
 #endif /* BITOPERATIONS_HPP_ */
