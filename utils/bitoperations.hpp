b8fbe51d78141e94c26496e62789513d8973e642
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-02-24 18:17:15 +0100

    use standard types and auto format

--- utils/bitoperations.hpp
+++ utils/bitoperations.hpp
@@ -1,77 +1,92 @@
 #ifndef BITOPERATIONS_HPP_
 #define BITOPERATIONS_HPP_
 
-#include <stdint.h>
-
 // ----- Basic bitwise operations ---------------------------------------------
 
-template <typename TYPE> inline TYPE AND(TYPE byte, TYPE mask) {
+template <typename TYPE>
+inline TYPE AND(TYPE byte, TYPE mask) {
     return (byte bitand mask);
 }
 
-template <typename TYPE> inline void AND_eq(TYPE &byte, TYPE mask) {
+template <typename TYPE>
+inline void AND_eq(TYPE &byte, TYPE mask) {
     byte and_eq mask;
 }
 
-template <typename TYPE> inline TYPE OR(TYPE byte, TYPE mask) {
+template <typename TYPE>
+inline TYPE OR(TYPE byte, TYPE mask) {
     return (byte bitor mask);
 }
 
-template <typename TYPE> inline void OR_eq(TYPE &byte, TYPE mask) {
+template <typename TYPE>
+inline void OR_eq(TYPE &byte, TYPE mask) {
     byte or_eq mask;
 }
 
-template <typename TYPE> inline TYPE XOR(TYPE byte, TYPE mask) {
+template <typename TYPE>
+inline TYPE XOR(TYPE byte, TYPE mask) {
     return (byte xor mask);
 }
 
-template <typename TYPE> inline void XOR_eq(TYPE &byte, TYPE mask) {
+template <typename TYPE>
+inline void XOR_eq(TYPE &byte, TYPE mask) {
     byte xor_eq mask;
 }
 
-template <typename TYPE> inline TYPE LEFT(uint8_t byte, uint8_t amount) {
+template <typename TYPE>
+inline TYPE LEFT(TYPE byte, int amount) {
     return (byte << amount);
 }
 
-template <typename TYPE> inline void LEFT_eq(uint8_t &byte, uint8_t amount) {
+template <typename TYPE>
+inline void LEFT_eq(TYPE &byte, int amount) {
     byte <<= amount;
 }
 
-template <typename TYPE> inline TYPE RIGHT(uint8_t byte, uint8_t amount) {
+template <typename TYPE>
+inline TYPE RIGHT(TYPE byte, int amount) {
     return (byte >> amount);
 }
 
-template <typename TYPE> inline void RIGHT_eq(uint8_t &byte, uint8_t amount) {
+template <typename TYPE>
+inline void RIGHT_eq(TYPE &byte, int amount) {
     byte >>= amount;
 }
 
-template <typename TYPE> inline TYPE INVERT(TYPE byte) {
+template <typename TYPE>
+inline TYPE INVERT(TYPE byte) {
     return (compl(byte));
 }
 
-template <typename TYPE> inline void INVERT_eq(TYPE &byte) {
+template <typename TYPE>
+inline void INVERT_eq(TYPE &byte) {
     byte = compl(byte);
 }
 
 // ----- Advanced bitwise operations ------------------------------------------
 
-template <typename TYPE> inline TYPE clearBit(TYPE byte, TYPE bit) {
+template <typename TYPE>
+inline TYPE clearBit(TYPE byte, TYPE bit) {
     return (byte bitand compl(LEFT<TYPE>(1, bit)));
 }
 
-template <typename TYPE> inline void clearBit_eq(TYPE &byte, TYPE bit) {
+template <typename TYPE>
+inline void clearBit_eq(TYPE &byte, TYPE bit) {
     byte bitand compl(LEFT<TYPE>(1, bit));
 }
 
-template <typename TYPE> inline TYPE setBit(TYPE byte, TYPE bit) {
+template <typename TYPE>
+inline TYPE setBit(TYPE byte, TYPE bit) {
     return (byte bitor (LEFT<TYPE>(1, bit)));
 }
 
-template <typename TYPE> inline void setBit_eq(TYPE &byte, TYPE bit) {
+template <typename TYPE>
+inline void setBit_eq(TYPE &byte, TYPE bit) {
     byte or_eq (LEFT<TYPE>(1, bit));
 }
 
-template <typename TYPE> inline bool readBit(TYPE byte, TYPE bit) {
+template <typename TYPE>
+inline bool readBit(TYPE byte, TYPE bit) {
     return (byte bitand (LEFT<TYPE>(1, bit)));
 }
 
