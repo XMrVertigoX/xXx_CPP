63a003d86db92aaf9b9cf277ae3bc51f26578d23
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-05-12 00:29:58 +0200

    Simplify base class

--- components/wireless/rf24/rf24_base.hpp
+++ components/wireless/rf24/rf24_base.hpp
@@ -4,22 +4,6 @@
 #include <xXx/components/wireless/rf24/rf24_types.hpp>
 #include <xXx/interfaces/ispi.hpp>
 
-// clang-format off
-
-#define R_REGISTER(...)         readRegister(__VA_ARGS__)
-#define W_REGISTER(...)         writeRegister(__VA_ARGS__)
-#define R_RX_PAYLOAD(...)       readRxPayload(__VA_ARGS__);
-#define W_TX_PAYLOAD(...)       writeTxPayload(__VA_ARGS__);
-#define FLUSH_TX(...)           flushTxFifo(__VA_ARGS__);
-#define FLUSH_RX(...)           flushRxFifo(__VA_ARGS__);
-#define REUSE_TX_PL(...)        reuseTxPayload(__VA_ARGS__);
-#define R_RX_PL_WID(...)        readRxPayloadLength(__VA_ARGS__);
-#define W_ACK_PAYLOAD(...)      cmd_W_ACK_PAYLOAD(__VA_ARGS__);
-#define W_TX_PAYLOAD_NOACK(...) cmd_W_TX_PAYLOAD_NOACK(__VA_ARGS__);
-#define NOP(...)                getStatus(__VA_ARGS__);
-
-// clang-format on
-
 namespace xXx {
 
 class RF24_BASE {
@@ -32,17 +16,17 @@ class RF24_BASE {
     RF24_BASE(ISpi &spi);
     virtual ~RF24_BASE();
 
-    uint8_t readRegister(RF24_Register reg, void *bytes, size_t numBytes);
-    uint8_t writeRegister(RF24_Register reg, void *bytes, size_t numBytes);
-    uint8_t readRxPayload(void *bytes, size_t numBytes);
-    uint8_t writeTxPayload(void *bytes, size_t numBytes);
-    uint8_t flushTxFifo();
-    uint8_t flushRxFifo();
-    uint8_t reuseTxPayload();
-    uint8_t readRxPayloadLength(uint8_t &payloadLength);
-    uint8_t cmd_W_ACK_PAYLOAD(uint8_t pipe, uint8_t bytes[], size_t numBytes);
-    uint8_t cmd_W_TX_PAYLOAD_NOACK(uint8_t bytes[], size_t numBytes);
-    uint8_t getStatus();
+    uint8_t R_REGISTER(RF24_Register reg, void *bytes, size_t numBytes);
+    uint8_t W_REGISTER(RF24_Register reg, void *bytes, size_t numBytes);
+    uint8_t R_RX_PAYLOAD(void *bytes, size_t numBytes);
+    uint8_t W_TX_PAYLOAD(void *bytes, size_t numBytes);
+    uint8_t FLUSH_TX();
+    uint8_t FLUSH_RX();
+    uint8_t REUSE_TX_PL();
+    uint8_t R_RX_PL_WID(uint8_t &payloadLength);
+    uint8_t W_ACK_PAYLOAD(uint8_t pipe, uint8_t bytes[], size_t numBytes);
+    uint8_t W_TX_PAYLOAD_NOACK(uint8_t bytes[], size_t numBytes);
+    uint8_t NOP();
 };
 
 } /* namespace xXx */
