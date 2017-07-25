d3699412912b5a7c1901064f62017cbadd5a7176
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-07-26 01:44:46 +0200

    Auto cast to underlying type

--- components/wireless/rf24/rf24_base.cpp
+++ components/wireless/rf24/rf24_base.cpp
@@ -1,14 +1,19 @@
 #include <assert.h>
 #include <string.h>
 
+#include <type_traits>
+
 #include <xXx/components/wireless/rf24/rf24_base.hpp>
 #include <xXx/components/wireless/rf24/rf24_types.hpp>
 #include <xXx/interfaces/ispi.hpp>
 #include <xXx/utils/bitoperations.hpp>
 
-#define DUMMY 0xFF
-#define COMMAND_LENGTH (1)
-#define __CAST(x) static_cast<uint8_t>(x)
+static const uint8_t dummyByte = 0xFF;
+
+template <typename TYPE>
+constexpr typename std::underlying_type<TYPE>::type asUnderlyingType(TYPE e) {
+    return (static_cast<typename std::underlying_type<TYPE>::type>(e));
+}
 
 namespace xXx {
 
@@ -26,7 +31,7 @@ uint8_t RF24_BASE::transmit(uint8_t command, const uint8_t *txBytes, uint8_t *rx
     if (txBytes != NULL) {
         memcpy(&buffer[1], txBytes, numBytes);
     } else {
-        memset(&buffer[1], DUMMY, numBytes);
+        memset(&buffer[1], dummyByte, numBytes);
     }
 
     _spi.transmit_receive(buffer, buffer, numBytes + 1);
@@ -41,77 +46,77 @@ uint8_t RF24_BASE::transmit(uint8_t command, const uint8_t *txBytes, uint8_t *rx
 }
 
 uint8_t RF24_BASE::R_REGISTER(RF24_Register address, uint8_t *bytes, size_t numBytes) {
-    uint8_t command = OR<uint8_t>(__CAST(RF24_Command::R_REGISTER), __CAST(address));
+    uint8_t command = OR<uint8_t>(asUnderlyingType(RF24_Command::R_REGISTER), asUnderlyingType(address));
     uint8_t status  = transmit(command, NULL, bytes, numBytes);
 
     return (status);
 }
 
 uint8_t RF24_BASE::W_REGISTER(RF24_Register address, const uint8_t *bytes, size_t numBytes) {
-    uint8_t command = OR<uint8_t>(__CAST(RF24_Command::W_REGISTER), __CAST(address));
+    uint8_t command = OR<uint8_t>(asUnderlyingType(RF24_Command::W_REGISTER), asUnderlyingType(address));
     uint8_t status  = transmit(command, bytes, NULL, numBytes);
 
     return (status);
 }
 
 uint8_t RF24_BASE::R_RX_PAYLOAD(uint8_t *bytes, size_t numBytes) {
-    uint8_t command = __CAST(RF24_Command::R_RX_PAYLOAD);
+    uint8_t command = asUnderlyingType(RF24_Command::R_RX_PAYLOAD);
     uint8_t status  = transmit(command, NULL, bytes, numBytes);
 
     return (status);
 }
 
 uint8_t RF24_BASE::W_TX_PAYLOAD(const uint8_t *bytes, size_t numBytes) {
-    uint8_t command = __CAST(RF24_Command::W_TX_PAYLOAD);
+    uint8_t command = asUnderlyingType(RF24_Command::W_TX_PAYLOAD);
     uint8_t status  = transmit(command, bytes, NULL, numBytes);
 
     return (status);
 }
 
 uint8_t RF24_BASE::FLUSH_TX() {
-    uint8_t command = __CAST(RF24_Command::FLUSH_TX);
+    uint8_t command = asUnderlyingType(RF24_Command::FLUSH_TX);
     uint8_t status  = transmit(command, NULL, NULL, 0);
 
     return (status);
 }
 
 uint8_t RF24_BASE::FLUSH_RX() {
-    uint8_t command = __CAST(RF24_Command::FLUSH_RX);
+    uint8_t command = asUnderlyingType(RF24_Command::FLUSH_RX);
     uint8_t status  = transmit(command, NULL, NULL, 0);
 
     return (status);
 }
 
 uint8_t RF24_BASE::REUSE_TX_PL() {
-    uint8_t command = __CAST(RF24_Command::REUSE_TX_PL);
+    uint8_t command = asUnderlyingType(RF24_Command::REUSE_TX_PL);
     uint8_t status  = transmit(command, NULL, NULL, 0);
 
     return (status);
 }
 
 uint8_t RF24_BASE::R_RX_PL_WID(uint8_t &payloadLength) {
-    uint8_t command = __CAST(RF24_Command::R_RX_PL_WID);
+    uint8_t command = asUnderlyingType(RF24_Command::R_RX_PL_WID);
     uint8_t status  = transmit(command, NULL, &payloadLength, 1);
 
     return (status);
 }
 
 uint8_t RF24_BASE::W_ACK_PAYLOAD(uint8_t pipe, const uint8_t *bytes, size_t numBytes) {
-    uint8_t command = OR<uint8_t>(__CAST(RF24_Command::W_ACK_PAYLOAD), pipe);
+    uint8_t command = OR<uint8_t>(asUnderlyingType(RF24_Command::W_ACK_PAYLOAD), pipe);
     uint8_t status  = transmit(command, bytes, NULL, numBytes);
 
     return (status);
 }
 
 uint8_t RF24_BASE::W_TX_PAYLOAD_NOACK(const uint8_t *bytes, size_t numBytes) {
-    uint8_t command = __CAST(RF24_Command::W_TX_PAYLOAD_NOACK);
+    uint8_t command = asUnderlyingType(RF24_Command::W_TX_PAYLOAD_NOACK);
     uint8_t status  = transmit(command, bytes, NULL, numBytes);
 
     return (status);
 }
 
 uint8_t RF24_BASE::NOP() {
-    uint8_t command = __CAST(RF24_Command::NOP);
+    uint8_t command = asUnderlyingType(RF24_Command::NOP);
     uint8_t status  = transmit(command, NULL, NULL, 0);
 
     return (status);
