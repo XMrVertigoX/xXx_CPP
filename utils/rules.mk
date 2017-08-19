a44254aa7087de0a254c4d266cec44d9ccdacbcc
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-08-19 15:12:08 +0200

    -

--- utils/rules.mk
+++ utils/rules.mk
@@ -10,21 +10,14 @@ RM      = rm -rf
 # ----- Directories and files -------------------------------------------------
 
 ifndef PROJECT_NAME
-PROJECT_NAME = PROJECT_NAME_not_set
+PROJECT_NAME = PROJECT_NAME_NOT_SET
 endif
 
-BINARY     = $(PROJECT_NAME).bin
-EXECUTABLE = $(PROJECT_NAME).elf
-HEXARY     = $(PROJECT_NAME).hex
-MAPFILE    = $(PROJECT_NAME).map
-
-ifndef OUTPUT_DIR
 OUTPUT_DIR = _out
-endif
 
-vpath %.bin $(OUTPUT_DIR)
-vpath %.elf $(OUTPUT_DIR)
-vpath %.hex $(OUTPUT_DIR)
+BINARY     = $(OUTPUT_DIR)/$(PROJECT_NAME).bin
+EXECUTABLE = $(OUTPUT_DIR)/$(PROJECT_NAME).elf
+HEXARY     = $(OUTPUT_DIR)/$(PROJECT_NAME).hex
 
 # ----- Flags -----------------------------------------------------------------
 
@@ -32,16 +25,17 @@ ASMFLAGS +=
 
 COMMON_CFLAGS += -fdata-sections
 COMMON_CFLAGS += -ffunction-sections
-# COMMON_CFLAGS += -fno-builtin
 COMMON_CFLAGS += -fno-exceptions
-COMMON_CFLAGS += -fno-strict-aliasing
 COMMON_CFLAGS += -fno-unwind-tables
+COMMON_CFLAGS += -fno-asynchronous-unwind-tables
 COMMON_CFLAGS += -nostdlib
+# Warnings
 COMMON_CFLAGS += -Wall
 # COMMON_CFLAGS += -Wextra
 
-CFLAGS +=
+CFLAGS += $(COMMON_CFLAGS)
 
+CXXFLAGS += $(COMMON_CFLAGS)
 CXXFLAGS += -fno-rtti
 CXXFLAGS += -fno-threadsafe-statics
 
@@ -50,20 +44,14 @@ CPPFLAGS += -MP
 CPPFLAGS += $(addprefix -D,$(SYMBOLS))
 CPPFLAGS += $(addprefix -I,$(realpath $(INCLUDE_DIRS)))
 
-ifeq ($(TOOLCHAIN_PREFIX), arm-none-eabi-)
-LDFLAGS += --specs=nano.specs
-LDFLAGS += --specs=nosys.specs
-endif
-
 LDFLAGS += -Wl,--gc-sections
-LDFLAGS += -Wl,-Map=$(OUTPUT_DIR)/$(MAPFILE)
+LDFLAGS += -Wl,-Map=$(OUTPUT_DIR)/$(PROJECT_NAME).map
 LDFLAGS += $(addprefix -L,$(realpath $(LIBRARY_DIRS)))
-
-_LIBFLAGS = $(addprefix -l,$(LIBRARIES))
+LDFLAGS += -Wl,--start-group $(addprefix -l,$(LIBRARIES)) -Wl,--end-group
 
 # ----- Sources and objects ---------------------------------------------------
 
-_SOURCE_FILES = $(sort $(realpath $(filter %.c %.cpp %.s %.S,$(SOURCE_FILES))))
+_SOURCE_FILES = $(realpath $(SOURCE_FILES))
 _OUTPUT_FILES = $(addprefix $(OUTPUT_DIR),$(basename $(_SOURCE_FILES)))
 
 _DEPENDENCY_FILES = $(addsuffix .d,$(_OUTPUT_FILES))
@@ -83,42 +71,45 @@ clean:
 
 # Output
 
-$(OUTPUT_DIR)/$(EXECUTABLE): $(_OBJECT_FILES)
+$(EXECUTABLE): $(_OBJECT_FILES)
+	echo $@
 	$(MKDIR) $(dir $@)
-	$(GCC) $(GCCFLAGS) $(LDFLAGS) $^ $(_LIBFLAGS) -o $@
+	$(GCC) $(GCCFLAGS) $(LDFLAGS) -o $@ $^
 
-$(OUTPUT_DIR)/$(BINARY): $(EXECUTABLE)
+$(BINARY): $(EXECUTABLE)
+	echo $@
 	$(MKDIR) $(dir $@)
 	$(OBJCOPY) -O binary $< $@
 
-$(OUTPUT_DIR)/$(HEXARY): $(EXECUTABLE)
+$(HEXARY): $(EXECUTABLE)
+	echo $@
 	$(MKDIR) $(dir $@)
 	$(OBJCOPY) -O ihex $< $@
 
 # Assembler
 
 $(OUTPUT_DIR)/%.o: /%.s $(MAKEFILE_LIST)
+	echo $@
 	$(MKDIR) $(dir $@)
 	$(GCC) $(GCCFLAGS) $(ASMFLAGS) $(CPPFLAGS) -c -o $@ $<
-	@echo $(shell realpath --relative-to=".." "$<")
 
 $(OUTPUT_DIR)/%.o: /%.S $(MAKEFILE_LIST)
+	echo $@
 	$(MKDIR) $(dir $@)
 	$(GCC) $(GCCFLAGS) $(ASMFLAGS) $(CPPFLAGS) -c -o $@ $<
-	@echo $(shell realpath --relative-to=".." "$<")
 
 # C
 
 $(OUTPUT_DIR)/%.o: /%.c $(MAKEFILE_LIST)
+	echo $@
 	$(MKDIR) $(dir $@)
-	$(GCC) $(GCCFLAGS) $(COMMON_CFLAGS) $(CFLAGS) $(CPPFLAGS) -c -o $@ $<
-	@echo $(shell realpath --relative-to=".." "$<")
+	$(GCC) $(GCCFLAGS) $(CFLAGS) $(CPPFLAGS) -c -o $@ $<
 
 # C++
 
 $(OUTPUT_DIR)/%.o: /%.cpp $(MAKEFILE_LIST)
+	echo $@
 	$(MKDIR) $(dir $@)
-	$(GCC) $(GCCFLAGS) $(COMMON_CFLAGS) $(CXXFLAGS) $(CPPFLAGS) -c -o $@ $<
-	@echo $(shell realpath --relative-to=".." "$<")
+	$(GCC) $(GCCFLAGS) $(CXXFLAGS) $(CPPFLAGS) -c -o $@ $<
 
 -include $(_DEPENDENCY_FILES)
